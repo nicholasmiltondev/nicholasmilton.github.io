b17bc0648be2e64b1114ea7935cac2621160481b
nicholasmiltondev <nicholassmilton@gmail.com>
2019-01-17 14:54:38 -0800

    changed button function

--- COMP4711/labs/2/quizQuestions/quizJSsource.js
+++ COMP4711/labs/2/quizQuestions/quizJSsource.js
@@ -1,9 +1,7 @@
 var zeroToFive = 5;
 
-function loopButtons(zeroToFive){
     for(let i = 1; i <= zeroToFive; i++){
         let btn = document.createElement("BUTTON");
         document.body.appendChild(btn);
         btn.innerHTML = 'test value';
     }
-}
