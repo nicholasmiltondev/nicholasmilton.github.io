2cae8e8e2ba8b506686d9cff268bbf9b52807759
nicholasmiltondev <nicholassmilton@gmail.com>
2019-01-23 10:57:07 -0800

    testing lambda again

--- COMP4711/labs/2/quizQuestions/quizJSsource.js
+++ COMP4711/labs/2/quizQuestions/quizJSsource.js
@@ -2,15 +2,14 @@ var askNumberOfQuestions = prompt("Enter number of questions you want to answer.
 if(askNumberOfQuestions == null || askNumberOfQuestions == ""){
     
 } else {
-    fetch().then(function(resp){
+    fetch("https://raw.githubusercontent.com/nicholasmiltondev/nicholasmiltondev.github.io/master/COMP4711/labs/2/quizQuestions/quiz.json").then(function(resp){
         return resp.json();
     }).then(function(data){
-        //for(let i = 1; i <= askNumberOfQuestions; i++){
-            //let btn = document.createElement("BUTTON");
-            //document.body.appendChild(btn);
-            //btn.innerHTML = i;
+        for(let i = 1; i <= askNumberOfQuestions; i++){
+            let btn = document.createElement("BUTTON");
+            document.body.appendChild(btn);
+            btn.innerHTML = i;
             //btn.innerHTML = data[i].buttons;
-        //}
-        document.body.appendChild("works");
+        }
     });    
 }
