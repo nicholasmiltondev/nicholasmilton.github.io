95a1451ec1e1b1523fea110dc47c5a683492eaf4
nicholasmiltondev <nicholassmilton@gmail.com>
2019-01-23 18:30:30 -0800

    updated buttons json

--- COMP4711/labs/2/buttons/buttons.js
+++ COMP4711/labs/2/buttons/buttons.js
@@ -1,9 +1,12 @@
-fetch("https://raw.githubusercontent.com/nicholasmiltondev/nicholasmiltondev.github.io/master/COMP4711/labs/2/quizQuestions/quiz.json").then(function(resp){
+
+
+fetch("https://raw.githubusercontent.com/nicholasmiltondev/nicholasmiltondev.github.io/master/COMP4711/labs/2/buttons/buttons.json").then(function(resp){
         return resp.json();
     }).then(function(data){
 for(let j = 0; j < 26; j++){
     let inputAlphabet = document.createElement("input");
     document.body.appendChild(inputAlphabet);
+
     inputAlphabet.type = "button";
     inputAlphabet.setAttribute("value", data.alphabet[j]);
     inputAlphabet.onclick = alertAlpha;
