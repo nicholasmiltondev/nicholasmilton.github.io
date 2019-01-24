5c8e2e523ebd2a85cc7f365498f821f6a1b85674
nicholasmiltondev <nicholassmilton@gmail.com>
2019-01-24 14:50:40 -0800

    adding local storage and admin page

---
+++ COMP4711/labs/2/quizQuestions/admin.js
@@ -0,0 +1,24 @@
+fetch("https://raw.githubusercontent.com/nicholasmiltondev/nicholasmiltondev.github.io/master/COMP4711/labs/2/quizQuestions/quiz.json").then(function(resp){
+        return resp.json();
+    }).then(function(data){
+        
+        console.log(data);
+         for(let i = 0; i < askNumberOfQuestions; i++){
+             var x = document.createElement("P");
+             x.innerHTML = data.quiz[i].q;
+             document.body.appendChild(x);
+
+            for(j in data.quiz[i].options){
+                let b = document.createElement("DIV");
+                document.body.appendChild(b);
+                let rbutton = document.createElement("input");
+                rbutton.type = "radio";
+                rbutton.name = "options" + i;
+                b.appendChild(rbutton);
+                
+                var z = document.createElement("label");
+                z.innerHTML = data.quiz[i].options[j];
+                b.appendChild(z);
+            }
+         }
+    });
