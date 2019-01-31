10556a40c4b37a6fad48c2f64fdf8e9255d702d4
nicholasmiltondev <nicholassmilton@gmail.com>
2019-01-30 22:03:58 -0800

    Created and almost finished lab 3

--- COMP4711/labs/3/quizQuestions/admin.js
+++ COMP4711/labs/3/quizQuestions/admin.js
@@ -1,24 +1,98 @@
-fetch("https://raw.githubusercontent.com/nicholasmiltondev/nicholasmiltondev.github.io/master/COMP4711/labs/2/quizQuestions/quiz.json").then(function(resp){
-        return resp.json();
-    }).then(function(data){
-        
-        console.log(data);
-         for(let i = 0; i < askNumberOfQuestions; i++){
-             var x = document.createElement("P");
-             x.innerHTML = data.quiz[i].q;
-             document.body.appendChild(x);
+function addQuestion() {
+    // Check local storage.
+    if (typeof (Storage) !== "undefined") {
+        // If exists, increment by 1.
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 0;
+        }
 
-            for(j in data.quiz[i].options){
-                let b = document.createElement("DIV");
-                document.body.appendChild(b);
+        // find current number to name question.
+        let num = localStorage.getItem("clickcount");
+        // Create div to hold Q&A.
+        let d = document.createElement("div");
+        document.body.appendChild(d);
+        // Create blank textbox for question.
+        let inputQuestion = document.createElement("input");
+        inputQuestion.type = "text";
+        inputQuestion.placeholder = "Enter a question" + num;
+        inputQuestion.name = "question" + num;
+        inputQuestion.id = "question" + num;
+        d.appendChild(inputQuestion);
+
+        // Create a container for each option
+        for (let i = 0; i < 4; i++) {
+            let qd = document.createElement("div");
+            d.appendChild(qd);
+
+            let rbutton = document.createElement("input");
+            rbutton.type = "radio";
+            rbutton.name = "options" + num;
+            rbutton.id = "option" + num + "a" + i;
+            rbutton.value = i;
+            qd.appendChild(rbutton);
+
+            let inputAnswer = document.createElement("input");
+            inputAnswer.type = "text";
+            inputAnswer.id = "answer" + num + "a" + i;
+            inputAnswer.placeholder = "Enter an answer";
+            qd.appendChild(inputAnswer);
+        }
+    }
+}
+
+function clearStorage() {
+    localStorage.clear();
+    location.reload();
+}
+
+function savePage() {
+    let num = localStorage.getItem("clickcount");
+    for (let k = 0; k < num; k++) {
+        localStorage.setItem('question' + k, document.getElementById('question' + k).value);
+        for (let m = 0; m < 4; m++) {
+            localStorage.setItem('option' + k + 'a' + m, document.getElementById('option' + k + 'a' + m).checked);
+            localStorage.setItem('answer' + k + 'a' + m, document.getElementById('answer' + k + 'a' + m).value);
+
+        }
+    }
+}
+
+function displaySavedQuestions() {
+    if (typeof (Storage) !== "undefined") {
+        // find current number to name question.
+        let num = localStorage.getItem("clickcount");
+        for (let t = 0; t < num; t++) {
+            // Create div to hold Q&A.
+            let d = document.createElement("div");
+            document.body.appendChild(d);
+            // Create blank textbox for question.
+            let inputQuestion = document.createElement("input");
+            inputQuestion.type = "text";
+            inputQuestion.value = localStorage.getItem("question" + t);
+            inputQuestion.name = "question" + t;
+            inputQuestion.id = "question" + t;
+            d.appendChild(inputQuestion);
+
+            // Create a container for each option
+            for (let i = 0; i < 4; i++) {
+                let qd = document.createElement("div");
+                d.appendChild(qd);
+
                 let rbutton = document.createElement("input");
                 rbutton.type = "radio";
-                rbutton.name = "options" + i;
-                b.appendChild(rbutton);
-                
-                var z = document.createElement("label");
-                z.innerHTML = data.quiz[i].options[j];
-                b.appendChild(z);
+                rbutton.name = "options" + t;
+                rbutton.id = "option" + t + "a" + i;
+                rbutton.checked = (localStorage.getItem("option" + t + "a" + i) === "true");
+                qd.appendChild(rbutton);
+
+                let inputAnswer = document.createElement("input");
+                inputAnswer.type = "text";
+                inputAnswer.id = "answer" + t + "a" + i;
+                inputAnswer.value = localStorage.getItem("answer" + t + "a" + i);
+                qd.appendChild(inputAnswer);
             }
-         }
-    });
+        }
+    }
+}
---
+++ COMP4711/labs/3/quizQuestions/user.js
@@ -0,0 +1,52 @@
+function displaySavedQuestions() {
+    if (typeof (Storage) !== "undefined") {
+        // find current number to name question.
+        let num = localStorage.getItem("clickcount");
+        for (let t = 0; t < num; t++) {
+            // Create div to hold Q&A.
+            let d = document.createElement("div");
+            document.body.appendChild(d);
+            // Create blank textbox for question.
+            let inputQuestion = document.createElement("p");
+            inputQuestion.innerHTML = localStorage.getItem("question" + t);
+            inputQuestion.id = "question" + t;
+            d.appendChild(inputQuestion);
+
+            // Create a container for each option
+            for (let i = 0; i < 4; i++) {
+                let qd = document.createElement("div");
+                qd.id = "div" + t + "a" + i;
+                d.appendChild(qd);
+
+                let rbutton = document.createElement("input");
+                rbutton.type = "radio";
+                rbutton.name = "options" + t;
+                rbutton.id = "option" + t + "a" + i;
+                rbutton.value = i;
+                qd.appendChild(rbutton);
+
+                let inputAnswer = document.createElement("p");
+                inputAnswer.id = "answer" + t + "a" + i;
+                inputAnswer.innerHTML = localStorage.getItem("answer" + t + "a" + i);
+                qd.appendChild(inputAnswer);
+            }
+        }
+    }
+}
+
+function markQuiz() {
+    let score = 0;
+    let num = localStorage.getItem("clickcount");
+    for (let t = 0; t < num; t++) {
+        for (let i = 0; i < 4; i++) {
+            if (document.getElementById('option' + t + 'a' + i).checked)
+                document.getElementById('div' + t + 'a' + i).style.backgroundColor = "red";
+            if(localStorage.getItem("option" + t + "a" + i) === "true"){
+                document.getElementById('div' + t + 'a' + i).style.backgroundColor = "green";
+                if(document.getElementById('option' + t + 'a' + i).checked)
+                    score++;
+            }
+        }
+    }
+    document.getElementById("finalScore").innerHTML="You got " + score + " out of " + num + "!";
+}
